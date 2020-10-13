1a15f3bf45a748e3dfe27ba7e827a3ee60287fff
fschechtman <gardenlover1000@gmail.com>
2020-10-12 20:11:50 -0400

    First commit

---
+++ src/build/precache-manifest.a2045f5e7930e03ad86540f1f3b317b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2666fae84566b09962eed8c69b753b72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fb94935541c0b6ec583",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "8520178d2c49134ee522",
+    "url": "/static/js/2.bc37484a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bc37484a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fb94935541c0b6ec583",
+    "url": "/static/js/main.62610865.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
